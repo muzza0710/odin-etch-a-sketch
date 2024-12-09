cd539fe2836c223cfb270c568b6e8fa021521883
odin <142325575+muzza0710@users.noreply.github.com>
2024-12-09 06:17:43 +0000

    added function: randomColor.
    toggles new color between red if off and a random colour if on

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const container = document.querySelector(".container");
-const resizeGridBtn = document.querySelector("button");
-let numOfRows = 16
+const resizeGridBtn = document.querySelector(".resizeGridBtn");
+const randomColourBtn = document.querySelector(".randomColourBtn");
+const lightToDarkkBtn = document.querySelector(".lightToDarkBtn");
+const initialOpacity = 0.5;
+const intialColor = `rgb(255 0 0/${initialOpacity})`;
+
+let randomColorIsActive = false;
+
+let numOfRows = 16;
 let width = container.offsetWidth / numOfRows;
+let color = `rgb(0 255 0/${initialOpacity})`;
+let newColor = "green";
 
 createGrid(numOfRows)
 
@@ -17,20 +26,32 @@ resizeGridBtn.addEventListener("click", () => {
     }
 })
 
+randomColourBtn.addEventListener("click", () => {
+    randomColorIsActive ? randomColourBtn.classList.remove("active"): randomColourBtn.classList.add("active")
+    randomColorIsActive = !randomColorIsActive;
+})
+
 function createGrid(size){
     for (let i = 0; i < size * size; i++) {
-        
         const cell = document.createElement("div");
         cell.classList.add("cell");
-        cell.style.background = "red";
-        // cell.style.border = "1px solid black";
+        cell.style.background = intialColor;
         cell.style.width = `${width}px`
         cell.style.height = `${width}px`
         cell.style.flex = `0 0 auto`;
         cell.addEventListener("pointerenter", () => {
-            cell.style.background = ("blue");
+            if (randomColorIsActive){
+                console.log(randomColorIsActive);
+                let r = Math.floor(Math.random() * 255).toString(16);
+                let g = Math.floor(Math.random() * 255).toString(16);
+                let b = Math.floor(Math.random() * 255).toString(16);
+                newColor = `#${r+g+b}`;
+            }
+            else {newColor ="red";
+                console.log(randomColorIsActive);
+            }
+            cell.style.background = newColor;
         });
-        
         container.appendChild(cell);
     }
 }
