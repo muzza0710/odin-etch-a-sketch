11490a77adaded31b899f1da87d2ec5163d32103
odin <142325575+muzza0710@users.noreply.github.com>
2024-12-09 02:53:45 +0000

    added event listener: for mouse over div.cells
    changes background color of cell from red to blue.
    also changed cells from buttons to divs

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const container = document.querySelector(".container");
 const numOfRows = 16
 let width = container.offsetWidth / numOfRows;
-console.log(width);
-
-// const width = parseInt(container.width) / 16;
 for (let i = 0; i < numOfRows * numOfRows; i++) {
     
-    const cell = document.createElement("button");
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
     cell.style.background = "red";
     cell.style.border = "1px solid black";
     cell.style.width = `${width}px`
     cell.style.height = `${width}px`
     cell.style.flex = `0 0 auto`;
+    cell.addEventListener("mouseover", () => {
+        cell.style.background = ("blue");
+    });
     
     container.appendChild(cell);
 }
